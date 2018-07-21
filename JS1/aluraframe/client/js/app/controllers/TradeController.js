a9d0e54a3cc1fcbedccc9c7c123990c77904b9d3
Maiara <maiaraoliveira.dejesus@gmail.com>
2018-07-21 20:43:37 -0300

    Translation to english

---
+++ JS1/aluraframe/client/js/app/controllers/TradeController.js
@@ -0,0 +1,19 @@
+class TradeController {
+
+    constructor() {
+  
+              let $ = document.querySelector.bind(document);
+  
+              this._inputData = $('#date');
+              this._inputQuantidade =  $('#quantity');
+              this._inputValor = $('#value');
+    }
+  
+    add(event) {
+          event.preventDefault();
+  
+  
+         
+  
+    }
+  }
