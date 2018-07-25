424d3c3d69795bac712fc586cd31d2d7189fff8c
Maiara <maiaraoliveira.dejesus@gmail.com>
2018-07-25 00:00:38 -0300

    Add funcional

--- JS1/aluraframe/client/js/app/controllers/TradeController.js
+++ JS1/aluraframe/client/js/app/controllers/TradeController.js
@@ -1,19 +1,30 @@
 class TradeController {
 
-    constructor() {
-  
-              let $ = document.querySelector.bind(document);
-  
-              this._inputData = $('#date');
-              this._inputQuantidade =  $('#quantity');
-              this._inputValor = $('#value');
-    }
-  
-    add(event) {
-          event.preventDefault();
-  
-  
-         
-  
-    }
-  }
+      constructor() {
+
+            let $ = document.querySelector.bind(document);
+
+            this._inputDate = $('#date');
+            this._inputQuantity = $('#quantity');
+            this._inputValue = $('#value');
+      }
+
+      add(event) {
+            event.preventDefault();
+
+            let date = new Date(...this._inputDate
+                  .value.split('-')
+                  .map(function (item, index) {
+                        return item - index % 2;
+                  }));
+
+            let trade = new Trade(
+                  date,
+                  this._inputQuantity.value,
+                  this._inputValue.value
+            );
+
+            console.log(trade);
+
+      }
+}
