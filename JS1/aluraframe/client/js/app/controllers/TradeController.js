4cd3cdfbb19a5d4e8948f21f3bfa3b2e5f608cdc
Maiara <maiaraoliveira.dejesus@gmail.com>
2018-07-26 00:09:34 -0300

    clear form

--- JS1/aluraframe/client/js/app/controllers/TradeController.js
+++ JS1/aluraframe/client/js/app/controllers/TradeController.js
@@ -7,16 +7,18 @@ class TradeController {
             this._inputDate = $('#date');
             this._inputQuantity = $('#quantity');
             this._inputValue = $('#value');
+            this._form = $("#tForm");
       }
 
       add(event) {
             event.preventDefault();
 
             let date = new Date(...this._inputDate
-                  .value.split('-')
-                  .map(function (item, index) {
-                        return item - index % 2;
-                  }));
+                  .value
+                  .split('-')
+                  .map((item, indice) => item - indice % 2)
+            );
+
 
             let trade = new Trade(
                   date,
@@ -25,6 +27,14 @@ class TradeController {
             );
 
             console.log(trade);
+            this.cleanForm();
+      }
 
+      cleanForm() {
+           this._inputDate.value = "";
+           this._inputDate.focus;
+           this._inputQuantity.value = "";
+           this._inputValue.value = "";
+           
       }
 }
